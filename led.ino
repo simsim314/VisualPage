71a4604739a66a74c57525fe7599e0572608e8c4
simsim314 <simsim314@gmail.com>
2019-09-05 20:01:13 +0300

    Update led.ino

--- led.ino
+++ led.ino
@@ -7,14 +7,40 @@
 const int redPin = 11;  // R petal on RGB LED module connected to digital pin 11 
 const int greenPin = 10;  // G petal on RGB LED module connected to digital pin 10 
 const int bluePin = 9;  // B petal on RGB LED module connected to digital pin 9
-const int onFor = 100;
+const int onFor = 1000;
 
 /**************************************************************************/
+
+void turn(unsigned char r, unsigned char g, unsigned char b)
+{
+    color(255, 255, 0); // turn the RGB LED red 
+    delay(onFor); // delay for 1 second  
+  
+    color(r,g,b); // turn the RGB LED green  
+    delay(onFor); // delay for 1 second  
+    
+}
 void setup()
 { 
   pinMode(redPin, OUTPUT); // sets the redPin to be an output 
   pinMode(greenPin, OUTPUT); // sets the greenPin to be an output 
   pinMode(bluePin, OUTPUT); // sets the bluePin to be an output
+  
+  color(255, 0, 255); // turn the RGB LED red 
+  delay(onFor); // delay for 1 second  
+  
+  
+  for(int i = 0; i < 10; i++)
+  {
+   turn(255,255,255);
+   turn(0,0,255);
+   turn(0,255,0);
+   turn(255,0,0);
+    
+  }
+  color(255, 0, 255); // turn the RGB LED red 
+  delay(onFor); // delay for 1 second  
+  
 }    
 /***************************************************************************/
 void loop()  // run over and over again  
@@ -24,14 +50,6 @@ void loop()  // run over and over again
 //char sequence = "YRGBWY"
 
   
-  color(255, 0, 0); // turn the RGB LED red 
-  delay(onFor); // delay for 1 second  
-  color(0,255, 0); // turn the RGB LED green  
-  delay(onFor); // delay for 1 second  
-  color(0, 0, 255); // turn the RGB LED blue  
-  delay(onFor); // delay for 1 second 
-  color(255, 255, 255); // turn the RGB LED blue  
-  delay(onFor); // delay for 1 second 
 
 }     
 /******************************************************/
