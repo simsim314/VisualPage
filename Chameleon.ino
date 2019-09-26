8d1aceccc80a50dac9b9a30cd5b0d44b4a7c392d
odotan <odotan@hotmail.com>
2019-09-26 18:55:08 +0300

    Create Chameleon.ino

---
+++ Chameleon.ino
@@ -0,0 +1,101 @@
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
+
+uint16_t ambient_light = 0;
+uint16_t red_light = 0;
+uint16_t green_light = 0;
+uint16_t blue_light = 0;
+
+#include <Adafruit_NeoPixel.h>
+
+Adafruit_NeoPixel strip(12, 16, NEO_GRB + NEO_KHZ800); 
+
+void setup() {
+
+  // Initialize Serial port
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println(F("--------------------------------"));
+  Serial.println(F("SparkFun APDS-9960 - ColorSensor"));
+  Serial.println(F("--------------------------------"));
+  
+  // Initialize APDS-9960 (configure I2C and initial values)
+  if ( apds.init() ) {
+    Serial.println(F("APDS-9960 initialization complete"));
+  } else {
+    Serial.println(F("Something went wrong during APDS-9960 init!"));
+  }
+  
+  // Start running the APDS-9960 light sensor (no interrupts)
+  if ( apds.enableLightSensor(false) ) {
+    Serial.println(F("Light sensor is now running"));
+  } else {
+    Serial.println(F("Something went wrong during light sensor init!"));
+  }
+  
+  // Wait for initialization and calibration to finish
+  delay(500);
+  // Initialize Serial port
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println(F("--------------------------------"));
+  Serial.println(F("SparkFun APDS-9960 - ColorSensor"));
+  Serial.println(F("--------------------------------"));
+  
+  // Initialize APDS-9960 (configure I2C and initial values)
+  if ( apds.init() ) {
+    Serial.println(F("APDS-9960 initialization complete"));
+  } else {
+    Serial.println(F("Something went wrong during APDS-9960 init!"));
+  }
+  
+  // Start running the APDS-9960 light sensor (no interrupts)
+  if ( apds.enableLightSensor(false) ) {
+    Serial.println(F("Light sensor is now running"));
+  } else {
+    Serial.println(F("Something went wrong during light sensor init!"));
+  }
+  
+  // Wait for initialization and calibration to finish
+  delay(500);
+  
+  // Initiate strip
+  strip.begin();
+
+
+}
+
+void loop() {
+  
+  // Read the light levels (ambient, red, green, blue)
+  if (  !apds.readAmbientLight(ambient_light) ||
+        !apds.readRedLight(red_light) ||
+        !apds.readGreenLight(green_light) ||
+        !apds.readBlueLight(blue_light) ) {
+    Serial.println("Error reading light values");
+  } else {
+    Serial.print("Ambient: ");
+    Serial.print(ambient_light);
+    Serial.print(" Red: ");
+    Serial.print(red_light);
+    Serial.print(" Green: ");
+    Serial.print(green_light);
+    Serial.print(" Blue: ");
+    Serial.println(blue_light);
+
+  for(uint16_t j = 0; j <= 12; j++)
+  {
+    strip.setPixelColor(j, red_light/10, green_light/10, blue_light/10);
+    strip.show();
+
+  }
+
+
+    
+  }
+  
+  // Wait 1 second before next reading
+  delay(200);
+}
